c30f114d884db5b53cd12f0c0c6e6d449e2e755f
Matt Carlson <matthewthomascarlson@gmail.com>
2016-05-03 23:45:03 +0000

    create user model, configure passport

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-var express = require("express");
-var app = express();
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/auth_demo_app");
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
 
+
+var app = express();
+mongoose.connect("mongodb://localhost/auth_demo_app");
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+    secret: "Robert was the best cat in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function(req, res) {
     res.render("home");
