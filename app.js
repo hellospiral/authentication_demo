90cac56464df55417021b0e0feb50fa143a5d8ca
Matt Carlson <matthewthomascarlson@gmail.com>
2016-05-04 00:09:46 +0000

    add register routes, add register form

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express               = require("express"),
 var app = express();
 mongoose.connect("mongodb://localhost/auth_demo_app");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(require("express-session")({
     secret: "Robert was the best cat in the world",
@@ -23,6 +24,9 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// ======================================
+// ROUTES
+// ======================================
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -30,6 +34,28 @@ app.get("/", function(req, res) {
 
 app.get("/secret", function(req, res) {
     res.render("secret");
+});
+
+// Auth Routes
+
+// show sign up form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+// handling user sign up
+app.post("/register", function(req, res) {
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/secret");
+        });
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
